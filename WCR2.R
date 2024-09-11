f907706f4dda4644312c96ff7e9e763c4d5b03aa
nat-martin <martaaq@bc.edu>
2024-09-10 22:45:07 -0400

    csv

--- WCR2.R
+++ WCR2.R
@@ -1,1 +1,14 @@
 #WCR2
+library(readr)
+data <- data.frame(
+  "Name" = c("natalie martin", "teddy grace", "marge kuffner"),
+  "Age" = c(20, 21, 22),
+  "Occupation" = c("Biologist", "Billionare ", "Plumber")
+)
+
+write.csv(data,"csv_file.csv")
+
+
+
+
+
